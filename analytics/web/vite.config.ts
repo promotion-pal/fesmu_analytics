cf2ca044b568302de5ed2eccda7d69098064821a
promotion-pal <promotion.work.pal@gmail.com>
2026-05-21 13:09:32 +1000

    test

--- analytics/web/vite.config.ts
+++ analytics/web/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
+// import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
+    // babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
 
